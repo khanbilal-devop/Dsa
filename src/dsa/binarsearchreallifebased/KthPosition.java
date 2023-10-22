2c00bb638176216795e7d77ae95d969e90d81601
Khan Ahmed <khan.ahmed@dspim.com>
2023-10-22 21:33:33 +0530

    Binary search on answer : Kth Position element

--- src/dsa/binarsearchreallifebased/KthPosition.java
+++ src/dsa/binarsearchreallifebased/KthPosition.java
@@ -1,2 +1,37 @@
-package dsa.binarsearchreallifebased;public class KthPosition {
+package dsa.binarsearchreallifebased;
+
+public class KthPosition {
+
+    public static void main(String[] args) {
+      int numbers[] = {2,3,4,7,11};
+      int k = 5;
+
+      int number = findKthPositive(numbers,k);
+        System.out.println("The answer is "+ number);
+    }
+
+    private static int findKthPositive(int[] numbers, int k) {
+        int start = 0;
+        int end = numbers.length -1;
+
+        if(k < numbers[start]){
+            return k;
+        }
+
+        while (start <= end){
+            int middle = (end - start)/2 + start;
+
+            int countMissingEL = numbers[middle] - (middle +1);
+            if(countMissingEL < k ){
+                start = middle +1;
+            }else{
+                end = middle -1;
+            }
+
+        }
+        return end + k +1;
+
+    }
+
+
 }
